b8caebeaa56c4d0bd11235df967f29897962ec66
scottpg <scottpgriffith@gmail.com>
2020-06-16 11:34:57 -0400

    App is in hooks, titlebar and description fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <h1>DreamState</h1>
     </div>
   );
-}
+};
 
 export default App;
