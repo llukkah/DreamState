7d0aed241260c988382a045cce77ee8491d7bb2e
llukkah <llukkah.rey@gmail.com>
2020-06-24 18:15:00 -0400

    Wake up component completed and functional

--- client/src/Components/WakeUp.jsx
+++ client/src/Components/WakeUp.jsx
@@ -4,49 +4,66 @@ import "./WakeUp.css";
 export default function WakeUp () {
     return (
     <div class="wakeup-body">
-        <p class="wakeup-mainheader">Wake Up. Get Out. </p>
-        <p class="wakeup-subheader">The following are links to donations sites, social justice organizations, etc.</p>
-<p class="wakeup-subheader">To add to the list, send your suggestions via the form on the contact page.</p>
+        <p class="wakeup-mainheader">Wake Up</p>
+        <p class="wakeup-subheader">A new way of staying active</p>
+        <p class="wakeup-subheader">To add to the list, send your suggestions via the form on the contact page.</p>
+        <p class="wakeup-header">There are multiple avenues to contribute to the movement for all ages and walks of life near and far. </p>
+        <p class="wakeup-info">Assess your time, skills and talents to what can be done.</p>
 
+        <p class="wakeup-info">Lean towards the creative form?  Sell your art and donate the proceeds to organizations to further the Black Lives Matter movement.</p>
 
-<a href="https://blacklivesmatter.com/" class="wakeup-links" target="_blank">Black Lives Matter</a>
-<a href="https://www.aclu.org/" class="wakeup-links" target="_blank">American Civil Liberties Union</a>
-<a href="https://www.joincampaignzero.org/" class="wakeup-links" target="_blank">Campaign Zero</a>
-<a href="https://www.naacp.org/" class="wakeup-links" target="_blank">National Association for the Advancement of Colored People</a>
-<a href="https://www.snap4freedom.org"  class="wakeup-links"target="_blank">SNaP Co</a>
+        <p class="wakeup-info">Are you a good cook?  Make meals for organizations, protesters, volunteers.</p>
 
-<a href="https://www.houseofgg.org" class="wakeup-links" target="_blank">House of GG</a>
-<a href="https://www.theokraproject.com" class="wakeup-links" target="_blank">The Okra Project</a>
-<a href="https://www.blackaids.org" class="wakeup-links" target="_blank">Black AIDS Institute</a>
-<a href="https://www.princessjanaeplace.org" class="wakeup-links" target="_blank">Princess Janae Place</a>
-<a href="https://www.youthbreakout.org" class="wakeup-links" target="_blank">Breakout!</a>
-<a href="https://www.tgijp.org" class="wakeup-links" target="_blank">TGI Justice</a>
-<a href="" class="wakeup-links" target="_blank">name</a>
-<a href="" class="wakeup-links" target="_blank">name</a>
-<a href="" class="wakeup-links" target="_blank">name</a>
-<a href="" class="wakeup-links" target="_blank">name</a>
-<a href="" class="wakeup-links" target="_blank">name</a>
+        <p class="wakeup-info">Excel at visual design / marketing?  Round up your friends and use resources to make flyers / banners.</p>
 
+        <p class="wakeup-info">Have a substantial following on Social Media?  Use your platform for calls to action and information.</p>
 
-<p>TheComprehensive, daily-updated list of petitions to sign, bail out funds to donate to across all states as well as additional resources to read up on:</p>
+        <p class="wakeup-info">Protest route is along your house or business?  Open your space to bathrooms, water, or safety if things go south.</p>
+        <hr></hr>
+        <div class="links-div">
+            <div class="wakeup-SJ-links">
+                <p class="wakeup-SJ-header">Social Justice Organizations:</p>
+                    <div class="inner-links-div">
+                        <a href="https://blacklivesmatter.com/" class="wakeup-links" target="_blank">Black Lives Matter</a>
+                        <a href="https://www.aclu.org/" class="wakeup-links" target="_blank">American Civil Liberties Union</a>
+                        <a href="https://www.naacp.org/" class="wakeup-links" target="_blank">National Association for the Advancement of Colored People</a>
+                        <a href="https://www.joincampaignzero.org/" class="wakeup-links" target="_blank">Campaign Zero</a>
+                        <a href="https://www.blackaids.org" class="wakeup-links" target="_blank">Black AIDS Institute</a>
+                        <a href="https://colorofchange.org/" class="wakeup-links" target="_blank">Color of Change</a>
+                        <a href="https://civilrights.org/" class="wakeup-links" target="_blank">Leadership Conference on Civil &#38; Human Rights</a>
+                        <a href="https://www.showingupforracialjustice.org/" class="wakeup-links" target="_blank">Showing Up For Racial Justice</a>
+                        <a href="https://nul.org/" class="wakeup-links" target="_blank">National Urban League</a>
+                        <a href="https://www.innocenceproject.org/" class="wakeup-links" target="_blank">Innocence Project</a>  
+                    </div>
+            </div>
 
-
-
-
-
-
-<p class="wakeup-header">There are multiple avenues to contribute to the movement for all ages and walks of life near and far. </p>
-<p class="wakeup-info">Assess your time, skills and talents to what can be done.</p>
-
-<p class="wakeup-info">Lean towards the creative form?  Sell your art and donate the proceeds to organizations to further the Black Lives Matter movement.</p>
-
-<p class="wakeup-info">Are you a good cook?  Make meals for organizations, protesters, volunteers.</p>
-
-<p class="wakeup-info">Excel at visual design / marketing?  Round up your friends and use resources to make flyers / banners.</p>
-
-<p class="wakeup-info">Have a substantial following on Social Media?  Use your platform for calls to action and information.</p>
-
-<p class="wakeup-info">Protest route is along your house or business?  Open your space to bathrooms, water, or safety if things go south.</p>
+            <div class="wakeup-SJ-links">
+                <p class="wakeup-SJ-header">Black LGBTQ Organizations:</p>
+                    <div class="inner-links-div">
+                        <a href="https://www.snap4freedom.org"  class="wakeup-links"target="_blank">SNaP Co</a>
+                        <a href="https://www.houseofgg.org" class="wakeup-links" target="_blank">House of GG</a>
+                        <a href="https://www.theokraproject.com" class="wakeup-links" target="_blank">The Okra Project</a>
+                        <a href="https://www.princessjanaeplace.org" class="wakeup-links" target="_blank">Princess Janae Place</a>
+                        <a href="http://www.youthbreakout.org/" class="wakeup-links" target="_blank">Breakout!</a>
+                        <a href="http://www.tgijp.org/" class="wakeup-links" target="_blank">TGI Justice</a>
+                        <a href="http://nbjc.org/" class="wakeup-links" target="_blank">National Black Justice Coalition</a>
+                        <a href="https://alp.org/" class="wakeup-links" target="_blank">The Audre Lorde Project</a>
+                        <a href="https://marshap.org/" class="wakeup-links" target="_blank">Marsha P. Johnson Institute</a>
+                        <a href="https://www.bravespacealliance.org/" class="wakeup-links" target="_blank">Brave Space Alliance</a>
+                        <a href="https://avp.org/" class="wakeup-links" target="_blank">Anti-Violence Project</a>
+                    </div>
+            </div>
+    
+            <div class="wakeup-SJ-links">
+                <p class="wakeup-SJ-header">Google Docs:</p>
+                <p class="wakeup-SJ-info">These docs contain a comprehensive list of Bail Out Funds across all states, GoFundMe pages for affected families from police brutality, petitions to sign,as well as additional resources that are updated daily:</p>
+                   <div class="inner-links-div">
+                        <a href="https://docs.google.com/document/d/1Xa9Av-NfuFsWBHlsMvPiqJHdNedZgnCRW56qAS-7PGQ/mobilebasic?urp=gmail_link" class="wakeup-links" target="_blank">Google Doc 1</a>
+                       <a href="https://docs.google.com/document/u/1/d/e/2PACX-1vSrT26HMWX-_hlLfiyy9s95erjkOZVJdroXYkU-miaHRk58duAnJIUWKxImRkTITsYhwaFkghS8sfIF/pub?mc_cid=113beb9657&mc_eid=0ad6ddc623#h.zaba4bh85ex" class="wakeup-links" target="_blank">Google Doc 2</a>
+                    </div>
+            </div>
         </div>
+{/* <a href="" class="wakeup-links" target="_blank">name</a> */}
+    </div>
     );
  }
