202fba78162de15c2ab530a5763f0ff6d1b2a3f4
llukkah <llukkah.rey@gmail.com>
2020-06-24 06:01:18 -0400

    Added Beauty Component

---
+++ client/src/Components/Beauty.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { getBeauty } from "../Services/api-helper";
+import "./Food.css";
+import { Link } from "react-router-dom";
+
+export default function Beauty () {
+    useEffect(() => {
+        const fetchData = async () => {
+            const resp = await getBeauty();
+            setBeauty(resp);
+        };
+        fetchData();
+    }, []);
+
+    const [allBeauty, setBeauty] = useState([]);
+
+    return (
+        <div>
+            <div className="interpret-list">
+                <Link to="/Interpret" class="interpret-link">
+                &#60;&#60;Back
+                </Link>
+                <h1 class="interpret-header">Beauty</h1>
+                <p class="interpret-disclaimer"> To add a company to the list, send your suggestions via the form on the contact page.</p>
+                <p class="interpret-disclaimer-2">Story, Mission Statement and/or About descriptions, taken directly from company’s website unless otherwise noted.</p>
+
+                {allBeauty.map((beauty, index) => (
+                <div key={index}>
+                    <div class="interpret-info">
+                        <a href={beauty.website_url} target="_blank"><img class="interpret-image" src={beauty.image_url}/></a>
+                        <div>
+                            <p class="interpret-name">{beauty.name}</p>
+                            <p class="interpret-description">{beauty.description}</p>
+                        </div>
+                    </div>
+                </div>
+                    ))}
+            </div>
+        </div>
+    );
+ }
+
+
+// # Food/Beauty/Podcasts/Fashion/Tech/Media: Story, Mission Statement and/or About descriptions, taken directly from company’s website unless otherwise noted.
+// # Books: Descriptions taken directly from Author's website unless otherwise noted.
--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "./Shared/Layout";
 import { Route } from "react-router-dom";
 import Food from "./Food.jsx";
+import Beauty from "./Beauty.jsx"
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <Layout>
         {/* <h1>DreamState</h1> */}
         <Route exact path="/Food" render={() => <Food />} />
+        <Route exact path="/Beauty" render={() => <Beauty />} />
       </Layout>
     </>
   );
